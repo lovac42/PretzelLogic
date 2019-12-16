e9539e838ac2d19c00cba4c61913521deb7f6837
lovac42 <lovac42@protonmail.com>
2019-12-16 02:23:03 -0500

    added css for locale lang code changes in 2.1.16.

--- src/zip.bat
+++ src/zip.bat
@@ -1,8 +1,18 @@
 @echo off
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=pretzel_logic
+set NAME=pretzel_logic
+set PACKID=1168065472
+set VERSION=0.0.5
 
-%ZIP% %REPO%_20.zip *.py %REPO%/*.*
 
+quick_manifest.exe "%NAME%" "%PACKID%" >%REPO%\manifest.json
+echo %VERSION% >%REPO%\VERSION
+
+fsum -r -jm -md5 -d%REPO% * > checksum.md5
+move checksum.md5 %REPO%\checksum.md5
+
+%ZIP% %NAME%_v%VERSION%_Anki20.zip *.py %REPO%\*
 cd %REPO%
-%ZIP% ../%REPO%_21.zip *.*
+%ZIP% ../%NAME%_v%VERSION%_Anki21.ankiaddon *
+%ZIP% ../%NAME%_v%VERSION%_CCBC.adze *
