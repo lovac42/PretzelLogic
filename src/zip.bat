b1ca83b0de30718f8c2773eb484c1181a8a156ae
lovac42 <lovac42@protonmail.com>
2020-04-01 18:21:45 -0400

    bump version to v0.0.6

--- src/zip.bat
+++ src/zip.bat
@@ -3,16 +3,20 @@ set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=pretzel_logic
 set NAME=pretzel_logic
 set PACKID=1168065472
-set VERSION=0.0.5
+set VERSION=0.0.6
 
 
-quick_manifest.exe "%NAME%" "%PACKID%" >%REPO%\manifest.json
 echo %VERSION% >%REPO%\VERSION
 
 fsum -r -jm -md5 -d%REPO% * > checksum.md5
 move checksum.md5 %REPO%\checksum.md5
 
 %ZIP% %NAME%_v%VERSION%_Anki20.zip *.py %REPO%\*
+
 cd %REPO%
+
+quick_manifest.exe "%NAME%" "%PACKID%" >manifest.json
 %ZIP% ../%NAME%_v%VERSION%_Anki21.ankiaddon *
+
+quick_manifest.exe "%NAME%" "%NAME%" >manifest.json
 %ZIP% ../%NAME%_v%VERSION%_CCBC.adze *
