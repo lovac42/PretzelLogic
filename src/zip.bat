6ee0656d49e8e2850554e2d42da9783f7bd3e323
lovac42 <lovac42@protonmail.com>
2020-04-24 22:09:27 -0400

    Added nightmode colors to default stylesheet

--- src/zip.bat
+++ src/zip.bat
@@ -1,9 +1,9 @@
 @echo off
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=pretzel_logic
-set NAME=pretzel_logic
+set NAME=PretzelLogic
 set PACKID=1168065472
-set VERSION=0.1.0
+set VERSION=0.1.1
 
 
 echo %VERSION% >%REPO%\VERSION
@@ -15,8 +15,8 @@ move checksum.md5 %REPO%\checksum.md5
 
 cd %REPO%
 
-quick_manifest.exe "%NAME%" "%PACKID%" >manifest.json
+quick_manifest.exe "Pretzel Logic: Retention Benchmarking" "%PACKID%" >manifest.json
 %ZIP% ../%NAME%_v%VERSION%_Anki21.ankiaddon *
 
-quick_manifest.exe "%NAME%" "%NAME%" >manifest.json
+quick_manifest.exe "Pretzel Logic: Retention Benchmarking" "%REPO%" >manifest.json
 %ZIP% ../%NAME%_v%VERSION%_CCBC.adze *
